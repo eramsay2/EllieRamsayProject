f237a09d4b2497c07ab64246f1bc0858676f51b6
Jack Myrick <contact@jackmyrick.design>
2025-07-14 22:14:11 +0100

    created new LeftHandPinchMenu script which detects the left hand pinching

---
+++ Assets/Ellie/Scripts/LeftHandPinchMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class LeftHandPinchMenu : MonoBehaviour
+{
+    [Header("Input Action Reference")]
+    public InputActionProperty leftIndexPressed;
+
+    [Header("Menu to Toggle")]
+    public GameObject menuUI;
+
+    private bool wasPinching = false;
+
+    void OnEnable()
+    {
+        leftIndexPressed.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        leftIndexPressed.action.Disable();
+    }
+
+    void Update()
+    {
+        // indexPressed is typically a float (0.0 to 1.0)
+        float pinchValue = leftIndexPressed.action.ReadValue<float>();
+        bool isPinching = pinchValue > 0.9f;
+
+        if (isPinching && !wasPinching)
+        {
+            ToggleMenu();
+        }
+
+        wasPinching = isPinching;
+    }
+
+    void ToggleMenu()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(!menuUI.activeSelf);
+        }
+    }
+}
