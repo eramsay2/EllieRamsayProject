97eff0c0372f3724ffdaf2f804ddd6cc083dc088
Jack Myrick <contact@jackmyrick.design>
2025-07-14 22:50:59 +0100

    created new EnableDisableHands script to show the hands when the pause menu is up

---
+++ Assets/Ellie/Scripts/EnableDisableHands.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
+
+public class EnableDisableHands : MonoBehaviour
+{
+    // This scripts enables or disables the hands during runtime
+    [Header("Components")]
+    [SerializeField] private XRInputModalityManager modalityManager;
+    [SerializeField] private GameObject LeftHand;
+    [SerializeField] private GameObject RightHand;
+    [SerializeField] private GameObject LeftController;
+    [SerializeField] private GameObject RightController;
+
+    public void ToggleHands()
+    {
+        modalityManager.enabled = (!modalityManager.enabled);
+        LeftHand.SetActive(!modalityManager.enabled);
+        RightHand.SetActive(!modalityManager.enabled);
+        LeftController.SetActive(!modalityManager.enabled);
+        RightController.SetActive(!modalityManager.enabled);
+    }
+}
